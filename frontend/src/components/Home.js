211c7b3650f1ab6f63362bc1c132d29e1788e6d2
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-05-02 00:27:59 -0400

    added Home.js in frontend

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Loader, Dimmer } from "semantic-ui-react";
+
+class Home extends Component {
+  state = {
+    msg: '',
+    loadingData: false,
+  }
+
+  async componentDidMount() {
+    this.setState({ loadingData: true });
+    document.title = "Fluz";
+    this.setState({ loadingData: false });
+  }
+
+  render() {
+    if (this.state.loadingData) {
+      return (
+        <Dimmer active inverted>
+          <Loader size='massive'>Loading...</Loader>
+        </Dimmer>
+      );
+    }
+
+    return (
+      <div>
+        <br />
+        <h1 style={{ "textAlign": "center" }}>Welcome!</h1>
+        <br /><br />
+        Hi There!
+        <br /><br />
+      </div>
+    );
+  }
+}
+
+export default Home;
